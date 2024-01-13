0f6171a5c1a15a86611ea6c75c0a625c6ab9c66d
Karthikraut <karthikraut2@gmail.com>
2024-01-13 12:21:09 +0530

    React and Firbase is installed

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import  React  from "react";
+import ReactDOM  from "react-dom";
+const App = ()=>{
+    return(
+        <h1 className="underline">Hello World</h1>
+    );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>);
+
+
